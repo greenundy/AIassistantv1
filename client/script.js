113dac70fd078391c13ae37f551eb4568cd1e28d
ITatRPR <andy@r-p-r.co.uk>
2023-01-19 18:04:13 +0000

    hotfix

--- client/script.js
+++ client/script.js
@@ -80,7 +80,7 @@ const handleSubmit = async (e) => {
 
   // fetch response from server
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('http://localhost:5005', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
