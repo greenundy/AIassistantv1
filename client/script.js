87d81ae18322989b1a947be7b2a49eacbdd3b423
ITatRPR <andy@r-p-r.co.uk>
2023-01-20 01:44:50 +0000

    url update

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   // fetch response from server
 
-  const response = await fetch('http://localhost:5005/', {
+  const response = await fetch('https://aiassistant.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
