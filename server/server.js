14167818dbd0bab99ea72227b31b296dbe45fe08
ITatRPR <andy@r-p-r.co.uk>
2023-03-04 18:33:10 +0000

    updated max tokens

--- server/server.js
+++ server/server.js
@@ -31,10 +31,11 @@ app.post('/', async (req, res) => {
             model: "text-davinci-003",
             prompt: `${prompt}`,
             temperature: 0,
-            max_tokens: 3000,
+            max_tokens: 300,
             top_p: 1,
             frequency_penalty: 0.5,
             presence_penalty: 0,
+            suffix: "..."
         });
 
         res.status(200).send({
